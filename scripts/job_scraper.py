dff03b22406206740ff54d0fcbe55e18cc5bae22
Kris Bruurs <krisbruurs@gmail.com>
2025-08-02 21:06:28 +0200

    fix

--- scripts/job_scraper.py
+++ scripts/job_scraper.py
@@ -18,11 +18,13 @@ def accept_cookies(driver_instance):
 
 def scrape_weworkremotely():
     options = uc.ChromeOptions()
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--headless") 
-    options.add_argument("--start-maximized") 
+    options.add_argument("--start-maximized") # Ensure the virtual window is maximized
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+
 
     driver = uc.Chrome(options=options)
     print("WebDriver initialized.")
